053f01108131e2f05f852e3350b8f63b79bb95d9
Rahul Yadav <rahul.z.yadav@here.com>
2020-11-29 17:53:00 +0100

    Make migrations and admin

--- flightApp/admin.py
+++ flightApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Passenger
+
 # Register your models here.
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ("firstName", "lastName", "middleName", "email", "phone")
+    list_per_page = 20
+
+
+admin.site.register(Passenger, PassengerAdmin)
