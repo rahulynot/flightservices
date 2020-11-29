3c40a6f57a727a4c6bc04192a503b56f4f148671
Rahul Yadav <rahul.z.yadav@here.com>
2020-11-29 17:32:45 +0100

    Create viewsets and configure url for them

--- flightApp/views.py
+++ flightApp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Flight, Passenger, Reservation
+from .serializers import FlightSerializer, PassengerSerializer, ReservationSerializer
+
 
 # Create your views here.
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    queryset = Passenger.objects.all()
+    serializer_class = PassengerSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
--- flightServices/urls.py
+++ flightServices/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from flightApp import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+router = DefaultRouter()
+router.register("flights", views.FlightViewSet)
+router.register("passengers", views.PassengerViewSet)
+router.register("reservations", views.ReservationViewSet)
+
+urlpatterns = [path("admin/", admin.site.urls), path("flightServices/", include(router.urls))]
