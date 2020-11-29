1c182d8d48d3ea88c795c96540edf6f427663492
Rahul Yadav <rahul.z.yadav@here.com>
2020-11-29 17:20:22 +0100

    Add data models

--- flightApp/models.py
+++ flightApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Flight(models.Model):
+    flightNumber = models.CharField(max_length=10)
+    airline = models.CharField(max_length=20)
+    departureCity = models.CharField(max_length=20)
+    arrivalCity = models.CharField(max_length=20)
+    dateOfDeparture = models.DateField()
+    timeOfDeparture = models.TimeField()
+
+
+class Passenger(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    middleName = models.CharField(max_length=20, blank=True)
+    email = models.CharField(max_length=20)
+    phone = models.CharField(max_length=10)
+
+
+class Reservation(models.Model):
+    flight = models.OneToOneField(Flight, on_delete=models.CASCADE)
+    passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
